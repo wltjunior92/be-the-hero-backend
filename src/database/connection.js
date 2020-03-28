6bdf27344de66c7623ce5639df17adf0706c0f08
Walter Junior <wlt.junior92@gmail.com>
2020-03-27 21:58:59 -0300

    Initial commit

---
+++ src/database/connection.js
@@ -0,0 +1,8 @@
+const knex = require('knex');
+const configurations = require('../../knexfile');
+
+const config = process.env.NODE_ENV === 'test' ? configurations.test : configurations.development;
+
+const connection = knex(config);
+
+module.exports = connection;
---
+++ tests/integration/ong.spec.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it('It should be able to create a new ONG', async () => {
+    const response = await request(app)
+      .post('/ongs')
+      .send({
+        name: 'Teste',
+        email: 'contato@email.com',
+        whatsapp: '61895487547',
+        city: 'Cidade Teste',
+        uf: 'UF',
+      });
+
+      expect(response.body).toHaveProperty('id');
+      expect(response.body.id).toHaveLength(8);
+  });
+});
